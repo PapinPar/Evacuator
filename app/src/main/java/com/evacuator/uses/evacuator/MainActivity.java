623fed87d1f6967f4324c47bb0dc5795c754e150
Papin <papironi1>
2016-01-05 11:21:35 +0200

    v 0.0.2 creating conect

--- app/src/main/java/com/evacuator/uses/evacuator/MainActivity.java
+++ app/src/main/java/com/evacuator/uses/evacuator/MainActivity.java
@@ -1,12 +1,22 @@
 package com.evacuator.uses.evacuator;
 
 import android.app.DialogFragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -41,6 +51,34 @@ public class MainActivity extends AppCompatActivity
                     Calendar = new DialogDate();
                     Calendar.show(getFragmentManager(), "Time");
                 }
+            }
+        });
+        request();
+
+    }
+
+    private void request()
+    {
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://app.bb-evacuator.ru/api/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        MyApi api = retrofit.create(MyApi.class);
+        Call<Users> usersCall = api.getOrder("1","123","12123");
+        usersCall.enqueue(new Callback<Users>() {
+            @Override
+            public void onResponse(Response<Users> response, Retrofit retrofit)
+            {
+                Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_SHORT).show();
+            }
+
+            public void onFailure(Throwable t)
+            {
+                Toast.makeText(getApplicationContext(), "BAD", Toast.LENGTH_SHORT).show();
             }
         });
     }

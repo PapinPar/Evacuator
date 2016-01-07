3c22473b9d027b9975e055f7f07e1f1c4885f633
3lolo <wopozniack@gmail.com>
2016-01-07 14:24:54 +0200

    giinit_0.1.0

---
+++ app/src/main/java/com/evacuator/uses/evacuator/OrderDetailsActivity.java
@@ -0,0 +1,11 @@
+package com.evacuator.uses.evacuator;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by root on 06.01.16.
+ */
+public class OrderDetailsActivity extends AppCompatActivity {
+
+}
